0b32ee5204a2f2d2705d470fd8067a1ecac8a5f8
rjjfajardo <rjjfajardo@gmail.com>
2021-10-11 23:43:54 +0800

    initial-commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# MobDev-Quiz
+Gist: a mobile application using flutter that allows for user input and implements navigation. 
+
+Instructions:
+
+Create a mobile application using flutter that allows for user input and implements navigation. You have the freedom to design the app however you want as long the following is implemented: 
+
+The app should have the following pages:
+
+Login Page (must have email and password fields)
+Dashboard
+Settings Page
+Profile Page
+
+Navigation Requirements:
+
+From Login Page to Dashboard
+After navigating to dashboard, dashboard page must not be able to go back to login page.
+From Dashboard to Settings and Dashboard to Profile Page
+After navigating to Settings page, Settings must be able to go back to Dashboard
+After navigating to Profile page, Profile must be able to go back to Dashboard
+From Settings to Login Page (Logout)
+When logging out, the app must navigate to the Login Page.
+Once in the login page, login must not be able to go back to Settings page.
+
---
+++ lib/screens/dashboard.dart
@@ -0,0 +1,98 @@
+/// Flutter code sample for AppBar
+
+// This sample shows an [AppBar] with two simple actions. The first action
+// opens a [SnackBar], while the second action navigates to a new page.
+
+import 'package:flutter/material.dart';
+import '/screens/login.dart';
+
+void main() => runApp(const MyApp());
+
+/// This is the main application widget.
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  static const String _title = 'Flutter Code Sample';
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: _title,
+      home: MyStatelessWidget(),
+    );
+  }
+}
+
+/// This is the stateless widget that the main application instantiates.
+class MyStatelessWidget extends StatelessWidget {
+  const MyStatelessWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    const IconData settings = IconData(0xe57f, fontFamily: 'MaterialIcons');
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Dashboard'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.settings),
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute<void>(
+                builder: (BuildContext context) {
+                  return Scaffold(
+                    appBar: AppBar(
+                      title: const Text('Settings'),
+                      actions: <Widget>[
+                        IconButton(
+                            icon: Icon(Icons.logout),
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => LoginPage()),
+                              );
+                            }),
+                      ],
+                    ),
+                    body: const Center(
+                      child: Text(
+                        'Settings Screen',
+                        style: TextStyle(fontSize: 24),
+                      ),
+                    ),
+                  );
+                },
+              ));
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.account_circle),
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute<void>(
+                builder: (BuildContext context) {
+                  return Scaffold(
+                    appBar: AppBar(
+                      title: const Text('Profile'),
+                    ),
+                    body: const Center(
+                      child: Text(
+                        'Profile Screen',
+                        style: TextStyle(fontSize: 24),
+                      ),
+                    ),
+                  );
+                },
+              ));
+            },
+          ),
+        ],
+      ),
+      body: const Center(
+        child: Text(
+          'This is the dashboard',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
